ec2428bde6538f0b4f36284fa6731a49fd2413e3
Oskar Hagen <oskar.hagen@outlook.com>
2023-10-13 23:39:28 +0200

    migate example to file
    In order to solve line problem (character overflow). This could be avoided with + but it was too long...

--- R/obs_filter.R
+++ R/obs_filter.R
@@ -17,33 +17,7 @@
 #' RasterStack of desired resolution and extent (WGS84).
 #' @return Data frame with two columns named 'x' and 'y' comprising
 #' the new set of observations filtered at grid resolution.
-#' @examples
-#' 
-#' # Load the European Alps extent and a raster of a random resolution
-#' data(geo_dat)
-#' data(exrst)
-#' 
-#' # Downloading in the European Alps the observations of two plant species
-#' obs.arcto = get_gbif("Arctostaphylos alpinus",geo=shp.lonlat)
-#' obs.saxi = get_gbif("Saxifraga cernua",geo=shp.lonlat)
-#' plot(vect(shp.lonlat))
-#' points(obs.arcto[,c("decimalLongitude","decimalLatitude")],pch=20,col="#238b4550",cex=1)
-#' points(obs.saxi[,c("decimalLongitude","decimalLatitude")],pch=20,col="#99000d50",cex=1)
-#' 
-#' # rbind both datasets
-#' both.sp = rbind(obs.arcto,obs.saxi)
-#' 
-#' # Run function
-#' obs.filt = obs_filter(both.sp,rst)
-#' 
-#' # Check new points
-#' x11();plot(vect(shp.lonlat))
-#' points(
-#' obs.filt[obs.filt$Species%in%"Arctostaphylos alpinus",c("x","y")],
-#' pch=20,col="#238b4550",cex=1)
-#' 
-#' points(obs.filt[obs.filt$Species%in%"Saxifraga cernua",c("x","y")],
-#' pch=20,col="#99000d50",cex=1)
+#' @example inst/examples/obs_filter_help.R
 #' 
 #' @export
 obs_filter=function(gbifs,grid)
---
+++ inst/examples/obs_filter_help.R
@@ -0,0 +1,26 @@
+
+# Load the European Alps extent and a raster of a random resolution
+data(geo_dat)
+data(exrst)
+
+# Downloading in the European Alps the observations of two plant species
+obs.arcto = get_gbif("Arctostaphylos alpinus",geo=shp.lonlat)
+obs.saxi = get_gbif("Saxifraga cernua",geo=shp.lonlat)
+plot(vect(shp.lonlat))
+points(obs.arcto[,c("decimalLongitude","decimalLatitude")],pch=20,col="#238b4550",cex=1)
+points(obs.saxi[,c("decimalLongitude","decimalLatitude")],pch=20,col="#99000d50",cex=1)
+
+# rbind both datasets
+both.sp = rbind(obs.arcto,obs.saxi)
+
+# Run function
+obs.filt = obs_filter(both.sp,rst)
+
+# Check new points
+x11();plot(vect(shp.lonlat))
+
+points(obs.filt[obs.filt$Species%in%"Arctostaphylos alpinus",c("x","y")],
+pch=20,col="#238b4550",cex=1)
+
+points(obs.filt[obs.filt$Species%in%"Saxifraga cernua",c("x","y")],
+pch=20,col="#99000d50",cex=1)
