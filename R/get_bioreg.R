4362ffb8525a0f29da1d03a1d9e10335d6cf6b68
Oskar Hagen <oskar.hagen@outlook.com>
2024-06-14 13:47:19 +0200

    new routine for download

---
+++ R/get_bioreg.R
@@ -0,0 +1,141 @@
+#' Bioreg Data Files
+#'
+#' A list of data files including download links, filenames, and descriptions.
+#'
+#' @format A list with each element containing:
+#' \describe{
+#'   \item{filename}{The name to save the downloaded file as.}
+#'   \item{link}{The URL to download the file.}
+#'   \item{description}{A short description of the file.}
+#' }
+#' @examples
+#' \dontrun{
+#' bioreg_list
+#' }
+#' @export
+bioreg_list <- list(
+  list(
+    filename = "eco_terra",
+    link = "https://www.arcgis.com/sharing/rest/content/items/b1636d640ede4d6ca8f5e369f2dc368b/data",
+    description = "Terrestrial Ecoregions of the World"
+  ),
+  list(
+    filename = "eco_fresh",
+    link = "https://www.arcgis.com/sharing/rest/content/items/41aa8662254f43699e792fa194d7b3bb/data",
+    description = "Freshwater Ecoregions of the World"
+  ),
+  list(
+    filename = "eco_marine",
+    link = "https://www.arcgis.com/sharing/rest/content/items/903c3ae05b264c00a3b5e58a4561b7e6/data",
+    description = "Marine Ecoregions of the World"
+  ),
+  list(
+    filename = "eco_hd_marine",
+    link = "https://datadownload-production.s3.amazonaws.com/WCMC036_MEOW_PPOW_2007_2012_v1.zip",
+    description = "High Definition Marine Ecoregions and Pelagic Provinces of the World (2007, 2012)"
+  )
+)
+
+
+#' Get Save Directory
+#'
+#' Determines the save directory within the package structure.
+#'
+#' @param save_dir Optional. The directory to save the downloaded files. Defaults to "inst/extdata" within the package structure.
+#' @return A string representing the save directory.
+#' @noRd
+get_save_dir <- function(save_dir = NULL) {
+  if (is.null(save_dir)) {
+    save_dir <- system.file("extdata", package = "gbif.range")
+    if (save_dir == "") {
+      save_dir <- file.path(getwd(), "inst", "extdata", "downloads")
+    }
+  }
+  return(save_dir)
+}
+
+
+
+#' Download Bioreg Data Files
+#'
+#' Downloads data files from a provided list of links and saves them to a specified directory.
+#'
+#' @param bioreg_name "all" or the filename of the desired bioregion to be downloaded. 
+#' See `bioreg_list` for available options. This contains a list of data file information with elements containing `link`, `filename`, and `description`.
+#' @param save_dir The directory to save the downloaded files. Defaults to "inst/extdata" within the package structure.
+#' @return NULL. Downloads the files to the specified directory.
+#' @examples
+#' \dontrun{
+#' get_bioreg(bioreg_list)
+#' }
+#' @export
+get_bioreg <- function(bioreg_name = "all", save_dir = NULL) {
+  save_dir <- get_save_dir(save_dir)
+  
+  # Ensure the directory exists
+  if (!dir.exists(save_dir)) {
+    dir.create(save_dir, recursive = TRUE)
+  }
+  
+  # Check if bioreg_name exists in bioreg_list
+  data_list <- bioreg_list
+  
+  if (bioreg_name != "all") {
+    # If bioreg_name does not exist, error
+    if (!bioreg_name %in% sapply(data_list, function(x) x$filename)) {
+      stop(paste("Bioregion", bioreg_name, "not found. \n Available bioregions are:", paste(sapply(data_list, function(x) x$filename), collapse = " ; ")))
+    }
+    data_list <- data_list[sapply(data_list, function(x) x$filename) == bioreg_name]
+    message("Preparing to download bioregion ", bioreg_name, " \n data file to: ", save_dir)
+  } else {
+    message("Preparing to download all listed bioregions: ", paste(sapply(data_list, function(x) x$filename), collapse = " ; "), " \n data files to: ", save_dir)
+  }
+  
+  # Download each file
+  for (data in data_list) {
+    # data <- data_list[[1]]
+    url <- data$link
+    destfile <- file.path(save_dir, paste0(data$filename, ".zip"))
+    
+    # Download the file
+    tryCatch({
+      download.file(url, destfile, mode = "wb")
+      message(paste("Downloaded:", data$filename))
+      if (!is.null(data$description)) {
+        message(paste("Description:", data$description))
+      }
+    }, error = function(e) {
+      message(paste("Failed to download:", data$filename, "\nError:", e$message))
+    })
+    
+    # Unzip to folder named after filename
+    unzip(destfile, exdir = file.path(save_dir, data$filename))
+  }
+}
+
+#' Check and Download Bioreg Data Files
+#'
+#' Checks if a directory exists and contains at least one .shp file. If not, calls get_bioreg to download the data.
+#'
+#' @param bioreg_name filename of the desired bioregion to be checked and downloaded if necessary. See `bioreg_list` for available options.
+#' @param save_dir The directory to save the downloaded files. Defaults to "inst/extdata" within the package structure.
+#' @return NULL. Checks and downloads the files to the specified directory if necessary.
+#' @examples
+#' \dontrun{
+#' check_and_get_bioreg("eco_terra")
+#' }
+#' @export
+check_and_get_bioreg <- function(bioreg_name = "eco_terra", save_dir = NULL) {
+  save_dir <- get_save_dir(save_dir)
+  
+  # Determine the directory to check
+  check_dir <- file.path(save_dir, bioreg_name)
+  
+  # Check if directory exists and contains at least one .shp file
+  if (!(dir.exists(check_dir) && length(list.files(check_dir, pattern = "\\.shp$", full.names = TRUE)) > 0)) {
+    message(bioreg_name, " directory does not exist or contains no .shp files. 
+            \n [", file.path(check_dir, bioreg_name),"] will be created and data will be downloaded.
+            \n Downloading data...")
+    get_bioreg(bioreg_name, save_dir)
+  }
+}
