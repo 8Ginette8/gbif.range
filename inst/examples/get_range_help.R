cc6fc256697f21f01d6f4ebf23461300c6eaff4f
Yohann Chauvier <43674773+8Ginette8@users.noreply.github.com>
2025-01-31 14:56:07 +0100

    Update get_range_help.R

--- inst/examples/get_range_help.R
+++ inst/examples/get_range_help.R
@@ -4,11 +4,11 @@ eco_terra <- read_bioreg(bioreg_name = "eco_terra", save_dir = NULL)
 
 # First download the worldwide observations of Panthera tigris from GBIF
 occ <- get_gbif(sp_name = "Panthera tigris",
-                   time_period = c(2000, 3000),
-                   basis = c("OBSERVATION",
-                             "HUMAN_OBSERVATION",
-                             "MACHINE_OBSERVATION",
-                             "OCCURRENCE"))
+                time_period = c(2000, 3000),
+                basis = c("OBSERVATION",
+                          "HUMAN_OBSERVATION",
+                          "MACHINE_OBSERVATION",
+                          "OCCURRENCE"))
 
 # Make range from occurance points
 range <- get_range(occ, eco_terra, "ECO_NAME", clustered_points_outlier = 4)
