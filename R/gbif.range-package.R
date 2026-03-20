5e97aad6ed23b16e7cc2d30a4abbc745879c09a2
Oskar Hagen <oskar.hagen@outlook.com>
2026-03-20 22:41:28 +0100

    adding a package level help
    ?gbif.range now should work

---
+++ R/gbif.range-package.R
@@ -0,0 +1,20 @@
+#' gbif.range: GBIF Wrapper and Ecoregion-based Species Range Mapping
+#'
+#' The `gbif.range` package provides tools to download, clean, and analyze GBIF
+#' occurrence data and to generate species range maps using ecoregion layers.
+#'
+#' Core capabilities include:
+#' - user-friendly GBIF retrieval wrappers
+#' - occurrence filtering for spatial analyses
+#' - ecoregion access and processing
+#' - range map generation and evaluation workflows
+#'
+#' Main user-facing functions include [get_gbif()], [get_gbif_count()],
+#' [get_range()], [get_status()], [get_doi()], [obs_filter()], [make_tiles()],
+#' [make_ecoreg()], [evaluate_range()], and [cv_range()].
+#'
+#' @name gbif.range
+#' @aliases gbif.range gbif.range-package package-gbif.range
+#' @docType package
+#' @keywords package
+"_PACKAGE"
